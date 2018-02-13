2aaf35fb4d1efe5aa29b770f1ceb130683ff88d1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-02-13 08:45:29 -0800

    Add cspace attribute to CodecNet

--- netmodels.py
+++ netmodels.py
@@ -1,5 +1,6 @@
 from torch.nn import Module, Sequential
 from torch.nn import Conv2d, ReLU
+from torch.nn.functional import sigmoid
 
 def classname(model):
     return model.__class__.__name__
@@ -12,12 +13,17 @@ class CodecNet(Module):
         inchan: number of input channels
         outchan: number of output channels
     """
-    def __init__(self, inchan, outchan):
+    def __init__(self, inchan, outchan, cspace="BW"):
         super(CodecNet, self).__init__()
         self.model = Sequential(
             Conv2d(inchan, 32, kernel_size=3, padding=1), ReLU(),
             Conv2d(32, outchan, kernel_size=3, padding=1)
         )
+        self.cspace = cspace
 
     def forward(self, x):
-        return self.model(x)
+        if self.cspace == "BW":
+            y = self.model(x)
+            return sigmoid(y)
+        else:
+            return self.model(x)
