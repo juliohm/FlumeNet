c39185cd328dee67d7b0d5b7dee4bf3aa273c9c1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-02-19 12:54:02 -0800

    Add video generation to main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from problems import VideoGenProblem
 from netmodels import CodecNet
 from torch.nn import MSELoss, BCELoss, L1Loss
+from plotting import movie
 
+# define the problem
 problem = VideoGenProblem("data/bw", ["data/bw/Run 3"], cspace="BW")
 
+# define the network model
 model = CodecNet(problem.pastlen()*problem.channels(),
                  problem.futurelen()*problem.channels(), cspace="BW")
 
+# define the loss criterion
 loss_fn = MSELoss() if problem.colorspace() == "RGB" else BCELoss()
 
 hyperparams = {
@@ -15,4 +19,8 @@ hyperparams = {
     "bsize": 64
 }
 
+# solve the problem with the model
 solution, losses = problem.solve(model, loss_fn, hyperparams)
+
+# generate a video with the trained network
+movie(solution, "data/bw/Run 3")
