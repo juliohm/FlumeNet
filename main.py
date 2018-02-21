8e64e451cb27f5a1c07caeb0581bbee87d30d1a7
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-02-20 17:39:53 -0800

    Add color space swtich in main.py

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from netmodels import CodecNet
 from torch.nn import MSELoss, BCELoss, L1Loss
 from plotting import movie
 
+# color space
+cspace = "BW"
+prefix = "data/bw/" if cspace == "BW" else "data/rgb/"
+
 # datasets
 traindirs = ["Run 1", "Run 2 - 1", "Run 2 - 2", "Run 3 - 2", "Run 4", "Run 5",
              "Run 6 - 1", "Run 6 - 2", "Run 7 - 1", "Run 7 - 2"]
 devdirs   = ["Run 3 - 1"]
 
-traindirs = ["data/bw/"+tdir for tdir in traindirs]
-devdirs   = ["data/bw/"+ddir for ddir in devdirs]
+traindirs = [prefix+tdir for tdir in traindirs]
+devdirs   = [prefix+ddir for ddir in devdirs]
 
 # define the problem
-problem = VideoGenProblem(traindirs, devdirs, cspace="BW")
+problem = VideoGenProblem(traindirs, devdirs, cspace=cspace)
 
 # define the network model
 model = CodecNet(problem.pastlen()*problem.channels(),
-                 problem.futurelen()*problem.channels(), cspace="BW")
+                 problem.futurelen()*problem.channels(), cspace=cspace)
 
 # define the loss criterion
 loss_fn = MSELoss() if problem.colorspace() == "RGB" else BCELoss()
@@ -31,4 +35,4 @@ hyperparams = {
 solution, losses = problem.solve(model, loss_fn, hyperparams)
 
 # generate a video with the trained network
-movie(solution, "data/bw/Run 1")
+movie(solution, prefix+"Run 1")
